b205dcb7df00c64b6c974dd691b1080108180122
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2022-05-02 18:46:02 -0400

    41454

--- App.js
+++ App.js
@@ -1,36 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import Basket from "./src/screens/Basket";
-import OrdersScreen from "./src/screens/OrdersScreen";
-import DishDetailsScreen from "./src/screens/DishDetailsScreen";
+import RootNavigator from "./src/navigation";
 
-import HomeScreen from "./src/screens/HomeScreen";
-import RestaurantDetailsScreen from "./src/screens/RestaurantDetailsScreen";
-import OrderDetails from "./src/screens/OrderDetails";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <HomeScreen /> */}
-      {/* <RestaurantDetailsScreen />
-       */}
-      {/* <DishDetailsScreen /> */}
-      <Basket />
-
-      {/* <OrdersScreen /> */}
-      {/* <OrderDetails /> */}
+    <NavigationContainer>
+      <RootNavigator />
       <StatusBar style="light" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    // padding: 10,
-    // paddingVertical: 30,
-  },
-});
